0df58105e0485123feab307e42e253287ba8a7c7
Chinonso Ani <achinonso@gmail.com>
2020-11-19 10:01:47 +0100

    multiple returns

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
-};
+  const [value, setValue] = useState(0)
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1)
+      setValue((prevState) => {
+        return prevState + 1
+      })
+    }, 2000)
+  }
 
-export default UseStateCounter;
+  return (
+    <>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+
+        <button className='btn' onClick={complexIncrease}>
+          Increase later
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default UseStateCounter
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users'
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
-};
+  const [users, setUsers] = useState([])
 
-export default UseEffectFetchData;
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    setUsers(users)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+  return (
+    <>
+      <h3>Github Users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt='' />
+              <h3>{login}</h3>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default UseEffectFetchData
